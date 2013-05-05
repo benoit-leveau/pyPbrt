69459561eea9a4ea33a9c36bba07bd596c69bb31
Benoît Leveau <benoit@mbpben.(none)>
2013-05-05 17:39:01 +0100

    Added new class: Normal Added new function: face_forward to flip a normal

--- core/geometry.py
+++ core/geometry.py
@@ -194,3 +194,97 @@ def distance_squared(p1, p2):
     """Return the squared distance between two points."""
     return (p1-p2).length_squared()
 
+
+class Normal(object):
+    
+    """Class holding a 3D Normal."""
+    
+    def __init__(self, x=0.0, y=0.0, z=0.0):
+        """Construct a Normal instance."""
+        self.x = float(x)
+        self.y = float(y)
+        self.z = float(z)
+    
+    @classmethod
+    def from_normal(cls, n):
+        """Construct a Normal from another Normal."""
+        return cls(n.x, n.y, n.z)
+
+    @classmethod
+    def from_vector(cls, v):
+        """Construct a Normal from a Vector."""
+        return cls(v.x, v.y, v.z)
+
+    def length_squared(self):
+        """Return the square value of the length of the normal."""
+        return self.x*self.x + self.y*self.y + self.z*self.z
+    
+    def length(self):
+        """Return the length of the normal."""
+        return math.sqrt(self.length_squared())
+    
+    def normalize(self):
+        """Returns a normalized (unit length) normal."""
+        return self/self.length()
+    
+    def __neg__(self):
+        """Overload the negation operator."""
+        return Normal(-self.x,
+                      -self.y,
+                      -self.z)
+    
+    def __mul__(self, f):
+        """Overload the multiplication operator."""
+        return Normal(self.x * f,
+                      self.y * f,
+                      self.z * f)
+    
+    def __rmul__(self, f):
+        """Overload the right multiplication operator."""
+        return Normal(self.x * f,
+                      self.y * f,
+                      self.z * f)
+    
+    def __div__(self, f):
+        """Overload the division operator."""
+        inv = 1.0 / f
+        return Normal(self.x * inv,
+                      self.y * inv,
+                      self.z * inv)
+    
+    def __eq__(self, n):
+        """Overload the comparison operator."""
+        return self.x == n.x and self.y == n.y and self.z == n.z
+    
+    def __ne__(self, n):
+        """Overload the comparison operator."""
+        return self.x != n.x or self.y != n.y or self.z != n.z
+    
+    def __getitem__(self, index):
+        """Overload the bracket operator.
+            
+            Example:
+            n1[0] will return n1.x
+            similarly for n1[1] and n1.y, n1[2] and n1.z
+            
+            """
+        if index == 0:
+            return self.x
+        elif index == 1:
+            return self.y
+        elif index == 2:
+            return self.z
+        else:
+            raise IndexError("list index out of range")
+    
+    def __str__(self):
+        """Return a string describing the vector."""
+        return "Normal " + '(' + str(self.x) + ',' + str(self.y) + ',' + str(self.z) + ')'
+
+
+def face_forward(n, v):
+    """Return the supplied normal flipped to the same hemisphere as v.""" 
+    if dot(n, v) < 0.0:
+        return -n
+    else:
+        return n
