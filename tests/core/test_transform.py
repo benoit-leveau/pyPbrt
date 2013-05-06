b37f0b6c6b28ec22ea1b6aea3a2f51c87d715d4d
Benoît Leveau <benoit@mbpben.(none)>
2013-05-06 23:04:58 +0100

    Added unit tests for Transform(Point) and Transform(Vector)

--- tests/core/test_transform.py
+++ tests/core/test_transform.py
@@ -1,7 +1,7 @@
 import unittest
 
-from core.geometry import Vector
-from core.transform import rotate_x, rotate_y, rotate_z, rotate
+from core.geometry import Point, Vector
+from core.transform import translate, scale, rotate_x, rotate_y, rotate_z, rotate
 
 
 class TestGeometry(unittest.TestCase):
@@ -16,6 +16,16 @@ class TestGeometry(unittest.TestCase):
                          rotate_y(20))
         self.assertEqual(rotate(70, Vector(0.0, 0.0, 1.0)),
                          rotate_z(70))
+
+    def test_transform(self):
+        self.assertEqual(translate(Point(10,10,10))(Point(1,2,3)),
+                         Point(11,12,13))
+        self.assertEqual(translate(Point(10,10,10))(Vector(1,2,3)),
+                         Vector(1,2,3))
+        self.assertEqual(scale(2,3,4)(Point(1,2,3)),
+                         Point(2,6,12))
+        self.assertEqual(scale(2,3,4)(Vector(1,2,3)),
+                         Vector(2,6,12))
 
 if __name__ == '__main__':
     unittest.main()
