d2026988749e1f5102a42cb708c01ac2472ac061
Benoit Leveau <benoit.leveau@primefocusworld.com>
2013-05-06 19:43:20 +0100

    New class: BSDFSample

---
+++ core/reflection.py
@@ -0,0 +1,26 @@
+"""BSDF Classes."""
+
+
+class BSDFSample(object):
+
+    """BSDFSample class."""
+
+    def __init__(self, up0, up1, ucomp):
+        """Default Constructor for BSDFSample."""
+        self.u_dir = [up0, up1]
+        self.u_component = ucomp
+
+    @abstractmethod
+    def from_rng(cls, rng):
+        """Constructs a random sample."""
+        return cls(rng.random_float(),
+                   rng.random_float(),
+                   rng.random_float())
+
+    def __str__(self):
+        """Return a string describing the sample."""
+        return "BSDFSample (u_dir=[%f, %f], u_component=%f)" % (self.u_dir[0],
+                                                                self.u_dir[1],
+                                                                self.u_component)
+        
+                   
