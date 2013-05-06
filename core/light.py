234c9ec6caa379283087a62ce3cd6ce8851f3213
Benoit Leveau <benoit.leveau@primefocusworld.com>
2013-05-06 19:43:09 +0100

    New (incomplete) classes: Light and LightSample

---
+++ core/light.py
@@ -0,0 +1,24 @@
+"""Light Classes."""
+
+
+class Light(object):
+
+    """Base Light Class."""
+
+    def __init__(self):
+        pass
+
+
+class LightSample(object):
+
+    """LightSample Class."""
+
+    def __init__(self):
+        pass
+
+    @abstractmethod
+    def from_rng(cls, rng):
+        ret = cls()
+        ret.u_pos[0] = rng.random_float()
+        ret.u_pos[1] = rng.random_float()
+        ret.u_component = rnd.random_float()
