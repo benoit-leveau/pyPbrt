c15fd03fbd86644fbc11e2e6ddee904d8174f5dc
Benoit Leveau <benoit.leveau@primefocusworld.com>
2013-05-06 17:35:55 +0100

    Added mockup of main function

---
+++ main.py
@@ -0,0 +1,23 @@
+
+def main():
+    options = Options()
+    filenames = []
+
+    # process command-line arguments
+
+    pbrt_init(options)
+
+    if not filenames:
+        # parse scene from standard input
+        parse_file("-")
+        
+    else:
+        # parse scene from input files
+        for filename in filenames:
+            if not parse_file(filename):
+                error("Couldn't open scene file \"%s\"" % filename)
+        
+    # process scene description
+
+    pbrt_cleanup()
+    
