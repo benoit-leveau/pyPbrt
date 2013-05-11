d60bf9c35e112df283aa3a39c1b21f4363e10c37
Benoît Leveau <benoit@mbpben.(none)>
2013-05-11 22:58:23 +0100

    Dummy unit tests for AnimatedTransform class

---
+++ tests/core/test_animated_transform.py
@@ -0,0 +1,34 @@
+import unittest
+
+from core.geometry import Point, Vector, Normal, Ray, RayDifferential, BBox
+from core.transform import AnimatedTransform, scale, translate, rotate
+
+
+class TestGeometry(unittest.TestCase):
+    
+    def setUp(self):
+        pass
+
+    def test_constructor(self):
+        pass
+
+    def test_interpolate(self):
+        pass
+
+    def test_motion_bounds(self):
+        pass
+
+    def test_has_scale(self):
+        pass
+
+    def test_call_ray(self):
+        pass
+
+    def test_call_point(self):
+        pass
+
+    def test_call_vector(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
