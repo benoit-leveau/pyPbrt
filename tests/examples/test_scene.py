3b05cc8241ed2db35ce169b67945891ebde74d78
Benoît Leveau <benoit@mbpben.(none)>
2013-05-13 04:02:04 +0100

    Test scene is starting to work

--- tests/examples/test_scene.py
+++ tests/examples/test_scene.py
@@ -3,7 +3,7 @@
 import sys
 
 from core.geometry import Point, Vector
-from core.transform import translate, look_at
+from core.transform import translate, look_at, Transform
 from core.primitive import GeometricPrimitive
 from core.scene import Scene
 from core.integrator import Integrator
@@ -13,32 +13,51 @@ from renderers.sampler_renderer import SamplerRenderer
 from integrators.whitted import WhittedIntegrator
 from film.image import ImageFilm
 from cameras.perspective import PerspectiveCamera
+from samplers.random import RandomSampler
+from filters.box import BoxFilter
 
 
-def create_scene():
-    """Create a default scene."""
-    
+def create_pyramid(primitives):
     # create the objects, with this layout (shown in 2D)
     #   O O O O  level 0, width 4, pos_x -6
     #    O O O   level 1, width 3, pos_x -4
     #     O O    level 2, width 2, pos_x -2
     #      O     level 3, width 1, pos_x 0
-    primitives = []
     material = None
-    for level in range(4):
-        width_array = 4 - level
-        start_pos = Point(-2.0*(3-level), -2.0*(3-level), level*2.0)
-        for i in range(width_array):
-            start_pos_i = start_pos + i * Vector(2.0, 0.0, 0.0)
-            for j in range(width_array):
-                pos = start_pos_i + j * Vector(0.0, 2.0, 0.0)
-                object_to_world = translate(pos)
-                world_to_object = object_to_world.inverse()
-                sphere = Sphere(object_to_world, world_to_object,
-                                False, 1.0, -1.0, 1.0, 360)
-                primitive = GeometricPrimitive(sphere, material, None)
-                primitives.append(primitive)
+    if False:
+        for level in range(4):
+            width_array = 4 - level
+            start_pos = Point(-2.0*(3-level), -2.0*(3-level), level*2.0)
+            for i in range(width_array):
+                start_pos_i = start_pos + i * Vector(2.0, 0.0, 0.0)
+                for j in range(width_array):
+                    pos = start_pos_i + j * Vector(0.0, 2.0, 0.0)
+                    object_to_world = translate(pos)
+                    world_to_object = object_to_world.inverse()
+                    sphere = Sphere(object_to_world, world_to_object,
+                                    False, 1.0, -1.0, 1.0, 360)
+                    primitive = GeometricPrimitive(sphere, material, None)
+                    primitives.append(primitive)
 
+
+def create_simple_sphere(primitives):
+    material = None
+    object_to_world = Transform()
+    world_to_object = Transform()
+    sphere = Sphere(object_to_world, world_to_object, False,
+                    1.0, -1.0, 1.0, 360)
+    primitive = GeometricPrimitive(sphere, material, None)
+    primitives.append(primitive)
+
+    
+def create_scene():
+    """Create a default scene."""
+
+    primitives = []
+
+    create_simple_sphere(primitives)
+    # create_pyramid(primitives)
+        
     # create the accelerator
     grid = GridAccel(primitives, False)
 
@@ -53,11 +72,9 @@ def create_scene():
     return scene
 
 
-def create_film(filename):
-    width = 640
-    height = 480
-    image_filter = None
-    crop = [0,0,0,0]
+def create_film(filename, width, height):
+    image_filter = BoxFilter(1.0, 1.0)
+    crop = [0,width-1,0,height-1]
     open_window = False
     film = ImageFilm(width, height, image_filter, crop, filename, open_window)
     return film
@@ -65,15 +82,15 @@ def create_film(filename):
 
 def create_camera(film):
     """Create a perspective camera."""
-    cam_pos = Point(20, 20, 5)
+    cam_pos = Point(10, 10, 0)
     cam_look = Point(0, 0, 0)
     cam_up = Vector(0, 0, 1) 
     cam_to_world = look_at(cam_pos, cam_look, cam_up)
-    screen_window = [0, 639, 0, 479]
+    screen_window = [0, film.x_resolution-1, 0, film.y_resolution-1]
     s_open = 0.0
     s_close = 0.5
-    lens_radius = 35.0
-    focal_distance = 35.0
+    lens_radius = 0.01
+    focal_distance = 0.1
     fov = 40.0
     camera = PerspectiveCamera(cam_to_world,
                                screen_window,
@@ -91,7 +108,11 @@ def create_renderer(camera):
     volume_integrator = Integrator()
 
     # sampler
-    sampler = None
+    sampler = RandomSampler(0, camera.film.x_resolution-1,
+                            0, camera.film.y_resolution-1,
+                            1,
+                            0.0, 0.5
+                            )
 
     # create the renderer
     sampler_renderer = SamplerRenderer(sampler,
@@ -106,13 +127,15 @@ def create_renderer(camera):
 def main():
     """Main function."""
 
-    if len(sys.argv) < 2:
-        print "Error. Specify filename."
+    if len(sys.argv) < 4:
+        print "Error. Specify filename, width, height."
         return -1
     filename = sys.argv[1]
-
+    width = int(sys.argv[2])
+    height = int(sys.argv[3])
+    
     scene = create_scene()
-    film = create_film(filename)
+    film = create_film(filename, width, height)
     camera = create_camera(film)
     renderer = create_renderer(camera)
 
