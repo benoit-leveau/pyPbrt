b9479710b081ae69ac3c0ad75da0e383ff49e857
Benoit Leveau <benoit.leveau@primefocusworld.com>
2013-05-13 17:04:17 +0100

    Making the script portable

--- cython/build_cython.sh
+++ cython/build_cython.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-export PYTHONPATH=/code/work/benoit/thirdparty/build/lib/python2.6/site-packages/:$PYTHONPATH
-export PATH=$PATH:/code/work/benoit/thirdparty/build/bin
-
 # compile
-pyfon setup.py build_ext --build-lib=build/pypbrt --build-temp=.build --cython-c-in-temp
+python setup.py build_ext --build-lib=build/pypbrt --build-temp=.build --cython-c-in-temp
 
 # install
 for folder in build/pypbrt/* ; do touch $folder/__init__.py ; done
