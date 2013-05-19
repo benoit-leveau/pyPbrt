fae26bd10a749c4512c72ec4f77ffb1a8fc9b30c
Benoît Leveau <benoit@mbpben.(none)>
2013-05-19 18:39:34 +0100

    Added several scenes in the test script

--- tests/examples/test_scene.py
+++ tests/examples/test_scene.py
@@ -17,13 +17,23 @@ from samplers.random import RandomSampler
 from filters.box import BoxFilter
 
 
-def create_pyramid(primitives):
+def create_pyramid():
+    primitives = []
+    # create the sphere primitive
+    material = None
+    object_to_world = Transform()
+    world_to_object = Transform()
+    sphere = Sphere(object_to_world, world_to_object, False, 1.0, -1.0,
+                    1.0, 360)
+    primitive = GeometricPrimitive(sphere, material, None)
+    
+    # now create the instances
     # create the objects, with this layout (shown in 2D)
     #   O O O O  level 0, width 4, pos_x -6
     #    O O O   level 1, width 3, pos_x -4
     #     O O    level 2, width 2, pos_x -2
     #      O     level 3, width 1, pos_x 0
-    material = None
+
     for level in range(4):
         width_array = 4 - level
         start_pos = Point(-2.0*(3-level), -2.0*(3-level), level*2.0)
@@ -31,15 +41,15 @@ def create_pyramid(primitives):
             start_pos_i = start_pos + i * Vector(2.0, 0.0, 0.0)
             for j in range(width_array):
                 pos = start_pos_i + j * Vector(0.0, 2.0, 0.0)
-                object_to_world = translate(pos)
-                world_to_object = object_to_world.inverse()
-                sphere = Sphere(object_to_world, world_to_object,
-                                False, 1.0, -1.0, 1.0, 360)
-                primitive = GeometricPrimitive(sphere, material, None)
-                primitives.append(primitive)
+                transform = translate(pos).inverse()
+                world_to_primitive = AnimatedTransform(transform, 0.0, transform, 1.0)
+                instance = TransformedPrimitive(primitive, world_to_primitive)
+                primitives.append(instance)
+    return primitives
 
 
-def create_simple_sphere(primitives):
+def create_simple_sphere():
+    primitives = []
     material = None
     object_to_world = translate(Point(0,1,0))
     world_to_object = object_to_world.inverse()
@@ -47,32 +57,14 @@ def create_simple_sphere(primitives):
                     1.0, -1.0, 1.0, 360)
     primitive = GeometricPrimitive(sphere, material, None)
     primitives.append(primitive)
+    return primitives
 
     
-def create_simple_sphere2(primitives):
-    material = None
-    object_to_world = Transform()
-    world_to_object = Transform()
-    sphere = Sphere(object_to_world, world_to_object, False,
-                    1.0, -1.0, 1.0, 360)
-    primitive = GeometricPrimitive(sphere, material, None)
-    transform = translate(Point(0,1,0))
-    world_to_primitive = AnimatedTransform(transform, 0.0, transform, 1.0)
-    transformed_primitive = TransformedPrimitive(primitive, world_to_primitive)
-    primitives.append(transformed_primitive)
-
-
-def create_scene():
+def create_scene(primitives):
     """Create a default scene."""
 
-    primitives = []
-
-    create_simple_sphere(primitives)
-    # create_pyramid(primitives)
-        
     # create the accelerator
-    # aggregate = GridAccel(primitives, False)
-    aggregate = primitives[0]
+    aggregate = GridAccel(primitives, False)
 
     # create the lights
     lights = []
@@ -93,12 +85,8 @@ def create_film(filename, width, height):
     return film
 
 
-def create_camera(film):
+def create_camera(film, cam_transform):
     """Create a perspective camera."""
-    cam_pos = Point(0, 3, 8)
-    cam_look = Point(0, 0.8, 0)
-    cam_up = Vector(0, 1, 0)
-    cam_transform = look_at(cam_pos, cam_look, cam_up)
     cam2world = cam_transform.inverse()
     frame = float(film.x_resolution) / float(film.y_resolution)
     if frame > 1.0:
@@ -148,16 +136,32 @@ def create_renderer(camera):
 def main():
     """Main function."""
 
-    if len(sys.argv) < 4:
-        print "Error. Specify filename, width, height."
+    if len(sys.argv) < 5:
+        print "Error. Specify filename, sene, width, height."
         return -1
     filename = sys.argv[1]
-    width = int(sys.argv[2])
-    height = int(sys.argv[3])
-    
-    scene = create_scene()
+    scene = sys.argv[2]
+    width = int(sys.argv[3])
+    height = int(sys.argv[4])
+
+    if scene == "sp":
+        primitives = create_simple_sphere()
+        cam_pos = Point(0, 3, 8)
+        cam_look = Point(0, 0.8, 0)
+        cam_up = Vector(0, 1, 0)
+    elif scene == "py":
+        primitives = create_pyramid()
+        cam_pos = Point(10, 10, 4)
+        cam_look = Point(0, 0, 2)
+        cam_up = Vector(0, 0, 1)
+    else:
+        print "unknown scene"
+        return -1
+        
+    cam_transform = look_at(cam_pos, cam_look, cam_up)
+    scene = create_scene(primitives)
     film = create_film(filename, width, height)
-    camera = create_camera(film)
+    camera = create_camera(film, cam_transform)
     renderer = create_renderer(camera)
 
     # launch render
